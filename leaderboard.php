bb39d5de4e26f7ae05312f6ee4fbdedfcc29370c
elementfish <elementfish@live.cn>
2013-07-18 05:03:15 -0700

    reload w/o timestamp

--- leaderboard.php
+++ leaderboard.php
@@ -1,40 +1,45 @@
-<? php
-	function cmp($a, $b){
-		return ( (double) $a -> Score < (double) $b -> Score)? -1:1;
-	}
-	function leaderboardsort($d){
-		$a = array();
-		foreach($d -> Record as $r){
-			$a[] = $r
-		}
-		usort($a, 'cmp');
-		return $a
-	}
+<?php
 
-	$q = $_GET["q"];
-	$xmlLboard = new DOMDocument();
-	$xmlLboard->load("leaderboard.xml");
-	
-	$dataLboard = $xmlLboard -> getElementsByTagName("Record");
-	
-	if (!empty($q))
-		for ($i = 0; $i < $dataLboard -> length; $i++){	
-			if ($dataLboard -> item($i) -> childNodes -> item(0) -> nodeValue == $q){
-				$dataLboard -> item($i) -> childNodes -> item(0) -> nodeValue += 5;
-				;
-				$xmlLboard -> saveXML($dataLboard -> item($i));
+
+
+$q = $_GET["name"]; // selected player
+$r = $_GET["flag"]; // flag of score increase
+
+$xmlLboard = simplexml_load_file("leaderboard.xml");
+
+if (!empty($q)&& $r==1)
+	for ($i = 0; $i < count($xmlLboard); $i++){	
+		if ($xmlLboard->Record[$i]->Name == $q){	//find player
+			
+			
+			$xmlLboard->Record[$i]->Score += 5;   // score increase
+			
+			// sort
+			$j = $i;
+			while ($j>0 && (double)$xmlLboard->Record[$j-1]->Score <= (double)$xmlLboard->Record[$j]->Score){
+				
+				$t = array(0 => (String) $xmlLboard->Record[$j-1]->Name, 1 => (Double) $xmlLboard->Record[$j-1]->Score);
+				$xmlLboard->Record[$j-1]->Name = $xmlLboard->Record[$j]->Name;
+				$xmlLboard->Record[$j-1]->Score = $xmlLboard->Record[$j]->Score;
+				$xmlLboard->Record[$j]->Name = $t[0];
+				$xmlLboard->Record[$j]->Score = $t[1];
+				$j--;
 			}
-	
-	
-	
-	$sortedData = leaderboardsort($dataLboard);
-	
-	for ($i = 0; $i < count($sortedData); $i++){
-		echo('<div class="player">');
-		echo('<span class="name">' .  $sortedData[i] -> Name . '</span>');
-		echo('<span class="score">' . $sortedData[i] -> Score . '</span>');
-		echo('</div>');
+			
+			$xmlLboard->asXML("leaderboard.xml");   // save xml
+		}
 	}
+	
+	
+// echo html
+	
+for ($i = 0; $i < count($xmlLboard->Record); $i++){
+	echo('<div class="player">');
+	echo('<span class="name">' .  $xmlLboard->Record[$i]->Name . '</span>');
+	echo('<span class="score">' . $xmlLboard->Record[$i]->Score . '</span>');
+	echo('</div>');
+}
+
 	
 	
 ?>
